5a9bff12487a9610675ed4b3d9dff3e563f28df7
7xge <7xge@fresnou.org>
2023-02-06 13:47:38 -0800

    initial commit

---
+++ DataTypes.js
@@ -0,0 +1,37 @@
+console.log(7);
+
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(7);
+
+//Floating Point Numbers (Floats)
+console.log(3.14);
+console.log(-0.123456);
+
+//strings
+//Characters, letters, symbols, spaces
+console.log("Hello World");
+console.log('$@!# is also a string');
+console.log(`backticks by the 1`);
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives *
+console.log(14/2);
+console.log(14%2); //% is called MOD
+                   //it means remainder after dividing
+
+//Concatenation means to combine strings
+console.log("my name"+" is Dr. Sins.");
+console.log('My'+' '+'age'+' '+'is'+14+'.');
+console.log(17%2);
+console.log(`17+2=${17+2}`);
+console.log(25>=20);
+console.log(25>100 && 25>100);
+console.log(25>100 || 25>10);
+console.log(3*2>1 || 25+71 !=17%5);
